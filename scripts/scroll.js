46d01750075c5a23f029267b2e56d14b6c7bb348
Andriy Kostiuk <Leo25334@gmail.com>
2022-01-21 12:27:15 +0200

    first commit

---
+++ scripts/scroll.js
@@ -0,0 +1,27 @@
+const headerLinks = document.querySelectorAll(".header-menu__item a");
+const linkCharacteristics = document.querySelector('.card-details__link-characteristics');
+
+seamless.polyfill();
+
+headerLinks.forEach((element) => {
+  element.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    smoothScroll(element);
+  });
+});
+
+linkCharacteristics.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  smoothScroll(linkCharacteristics);
+})
+
+let smoothScroll = function(element) {
+  const id = element.getAttribute("href").substring(1);
+    const section = document.getElementById(id);
+    if(section) {
+      seamless.elementScrollIntoView(section, {
+        behavior: 'smooth',
+        block: 'start'
+      })
+    } 
+}
